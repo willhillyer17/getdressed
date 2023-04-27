80f569487757b192a44c21f39adee683392b9c32
willhillyer17 <131925794+willhillyer17@users.noreply.github.com>
2023-04-26 20:57:56 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,220 @@
+const tops = {
+  "Crop Top": 5,
+  "T-Shirt": 4,
+  "Mesh Top": 3,
+  "Sweater": 4,
+  "Cardigan": 3,
+  "Hoodie": 2,
+  "None": 2
+};
+
+const bottoms = {
+  "Jeans": 5,
+  "Leggings": 1,
+  "Skirt": 3,
+  "Shorts": 3,
+  "Mini Skirt": 4,
+  "Bootie Shorts": 2,
+    "None": 2
+};
+
+const bras = {
+  "Push-Up Bra": 3,
+  "Sports Bra": 2,
+  "None": 2,
+  "Strapless Bra": 1,
+  "Bralette": 1,
+  "Wireless Bra": 2,
+  "T-Shirt Bra": 3
+};
+
+const thighHighs = {
+  "Opaque": 3,
+  "Sheer": 4,
+  "Fishnet": 2,
+  "Lace": 3,
+  "Patterned": 2,
+  "None": 2,
+
+  "Stay-Up": 4
+};
+
+const underwear = {
+  "Throng": 3,
+  "Bikini": 4,
+  "Boyshorts": 2,
+  "Briefs": 3,
+  "High-Waisted": 2,
+  "None": 2,
+  "High-Waisted Throng": 4
+};
+
+const makeup = [
+  {
+    name: "None",
+    weight: 1,
+    items: []
+  },
+  {
+    name: "Minimal",
+    weight: 2,
+    items: [
+      "Mascara",
+      "Lipstick",
+      "Concealer"
+    ]
+  },
+  {
+    name: "Average",
+    weight: 3,
+    items: [
+      "Mascara",
+      "Eyeliner",
+      "Eyeshadow",
+      "Blush",
+      "Lipstick",
+      "Concealer",
+      "Foundation"
+    ]
+  },
+  {
+    name: "Above average",
+    weight: 4,
+    items: [
+      "Mascara",
+      "Eyeliner",
+      "Eyeshadow",
+      "Blush",
+      "Lipstick",
+      "Lip liner",
+      "Concealer",
+      "Foundation",
+      "Highlighter",
+      "Bronzer",
+      "Eyebrows"
+    ]
+  },
+  {
+    name: "A lot",
+    weight: 5,
+    items: [
+      "Mascara",
+      "Eyeliner",
+      "Fake lashes",
+      "Eyeshadow",
+      "Blush",
+      "Eyebrows",
+      "Lipstick",
+      "Lip gloss",
+      "Lip liner",
+      "Concealer",
+      "Foundation",
+      "Highlighter",
+      "Bronzer",
+      "Fake or painted nails"
+    ]
+  }
+];
+
+const cam = [
+  {
+    name: "Face",
+    weight: 3
+  },
+  {
+    name: "Bobs",
+    weight: 2
+  },
+  {
+    name: "Mouth",
+    weight: 2
+  },
+  {
+    name: "pAss",
+    weight: 1
+  },
+  {
+    name: "Neck",
+    weight: 1
+  }
+];
+
+const activities = [
+  {
+    name: "Pictures",
+    weight: 2
+  },
+  {
+    name: "Video",
+    weight: 3
+  },
+  {
+    name: "Restraints",
+    weight: 4
+  },
+  {
+    name: "Oiled",
+    weight: 3
+  },
+  {
+    name: "Fake Cam",
+    weight: 2
+  },
+  {
+    name: "Fake member",
+    weight: 1
+  }
+];
+
+const accessories = [
+  {
+    name: "Cat ears",
+    weight: 3
+  },
+  {
+    name: "Choker",
+    weight: 4
+  },
+  {
+    name: "Fake Piercing",
+    weight: 2
+  },
+  {
+    name: "Fake Tattoo",
+    weight: 1
+  },
+  {
+    name: "Collar",
+    weight: 2
+  },
+  {
+    name: "Your Choice",
+    weight: 3
+  }
+];
+
+
+const optionalAesthetic = {
+  "Cottage": 3,
+  "Egirl": 2,
+  "Goth": 2,
+  "Grunge": 3,
+  "Kawaii": 3,
+  "Pastel Goth": 3,
+  "Bimbo": 1,
+  "Whimsey": 2,
+};
+
+const mainColor = {
+  "White": 4,
+  "Black": 4,
+  "Grey": 3,
+  "Yellow": 2,
+  "Red": 2,
+  "Blue": 3,
+  "Green": 2,
+  "Brown": 2,
+  "Pink": 3,
+  "Orange": 1,
+  "Purple": 2,
+};
